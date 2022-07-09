e3bb923684d116178c2a694c50c5af8a0d8ab1ac
Leonieke <datwatikmaak@gmail.com>
2022-07-09 14:02:13 +0200

    removed unused file and added tests

--- pybitespodcast/search.py
+++ pybitespodcast/search.py
@@ -11,16 +11,22 @@ Link: {link}
 Episode = namedtuple("Episode", "title link")
 
 
+def _strip_html(text):
+    return re.sub("<[^<]+?>", "", text)
+
+
+def _get_pybites_link(link):
+    slug = re.sub(r".*.com/(.*)\.mp3.*", r"\1", link)
+    return f"{PODCAST_BASE_URL}/{slug}"
+
+
+def _print_episode(episode):
+    return EPISODE_MATCH.format(title=episode.title, link=episode.link)
+
+
 class FeedSearcher:
     def __init__(self, feed=PYBITES_FEED):
         self.entries = feedparser.parse(feed).entries
-
-    def _strip_html(self, text):
-        return re.sub("<[^<]+?>", "", text)
-
-    def _get_pybites_link(self, link):
-        slug = re.sub(r".*.com/(.*).mp3.*", r"\1", link)
-        return f"{PODCAST_BASE_URL}/{slug}"
 
     def search(self, term):
         for entry in self.entries:
@@ -28,15 +34,12 @@ class FeedSearcher:
             term = term.lower()
             if term in entry.summary.lower() or term in title.lower():
                 link = entry.links[0]["href"]
-                link = self._get_pybites_link(link)
+                link = _get_pybites_link(link)
                 yield Episode(title, link)
-
-    def _print_episode(self, episode):
-        return EPISODE_MATCH.format(title=episode.title, link=episode.link)
 
     def __call__(self):
         while True:
-            term = input("Search for episode ('q' for exit): ")
+            term = input("Search for episodes ('q' for exit): ")
             term = term.strip().lower()
             if term == "q":
                 print("Bye")
@@ -46,11 +49,7 @@ class FeedSearcher:
                 print("No hits, search again")
                 continue
             for episode in matching_episodes:
-                print(self._print_episode(episode))
-
-            print(
-                f"There are {len(matching_episodes)} episodes found with the term: {term}"
-            )
+                print(_print_episode(episode))
 
 
 def main():
